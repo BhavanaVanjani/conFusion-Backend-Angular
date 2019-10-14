cb9ef9a161cd4ec38b5ad9b066273219fa902670
BhavanaVanjani <bhavana.vanjani@gmail.com>
2019-10-14 15:07:23 +0530

    Firebase as BaaS

---
+++ conFusion-Angular6-Firebase/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD1prFKB_748USM-ASfLYYb45s0e5jvXVM",
+    authDomain: "confusionserver-db93a.firebaseapp.com",
+    databaseURL: "https://confusionserver-db93a.firebaseio.com",
+    projectId: "confusionserver-db93a",
+    storageBucket: "confusionserver-db93a.appspot.com",
+    messagingSenderId: "381960319445",
+    appId: "1:381960319445:web:b0f5d90ec88bbd17e3cd5c",
+    measurementId: "G-T91CS9K9RK"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
